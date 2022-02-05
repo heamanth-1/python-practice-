a3c53a07932d227f2e70c4b6e1a7123f88bdad8e
heamanth-1 <heamnathgreat@gmail.com>
2022-02-05 16:03:11 +0530

    first commit

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 months = ["jan","feb","march","april","may","june","july","august","september","october","november","december"]
-count = 0
+count = 1
+matrix = int(input("choose the matrix format \n 1.2X6 press 1 \n 2.3X4 press 2 \n 3.4X3 press 3 \n enter your choice: "))
 print("[",end="")
 for m in months :
     count+=1
-    if count > 4 :
+    if count > matrix + 1 :
         count=1
         print(m)
     else:
